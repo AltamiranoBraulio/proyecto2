4b855a42d841473c73444da42cc23bc2a2fab9d5
AltamiranoBraulio <143303555+AltamiranoBraulio@users.noreply.github.com>
2023-10-27 10:36:35 +0000

    Avances 2p

---
+++ include/Juego.hpp
@@ -0,0 +1,14 @@
+#pragma once
+#include <Vehiculo.hpp>
+#include <Etapa.hpp>
+
+class Juego
+{
+private:
+    Dinosaurio dinosauri; // tiene
+    Escenario escenario;
+
+public:
+    Juego(/* args */) {}
+    ~Juego() {}
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,52 @@
-#include <iostream>
-#include <Mascota.hpp>
+#include <Ventana.hpp>
+#include <Dibujo.hpp>
+#include <curses.h>
+#include <unistd.h>
 
 int main(int argc, char const *argv[])
 {
-    std::cout << "Juego de mascotas" << std::endl;
 
-    Mascota m1("Firulais");
+    Ventana v;
 
-    m1.Jugar();
-    m1.Jugar();
-    m1.Jugar();
+    Dibujo d1(1, 100, "fondo");
+    Dibujo d2(1, 12, "hillclimber");
+    // Dibujo d3(50, 10, "car");
+    // Dibujo d4(5, 1, "Dinosaurio");
+    // Dibujo d5(100, 1, "LogoJurasic");
 
-    std::cout << m1.DecirNombre()
-              << "tiene" << m1.DecirHambre() << "de hambre.";
+    bool ejecucion = true;
+    while (ejecucion)
+    {
 
-    m1.Comer(5);
+        v.Actualizar();
+        if (getch() == 'q')
+        {
+            ejecucion = false;
+        }
+        if (getch() == 'd')
+        {
+            d2.AvanzarX(1);
+           
+        }
+        if (getch() == 'a')
+        {
+            d2.RetrocederX(1);
+           
+        }
+        clear();
+        d1.Dibujar();
+        d2.Dibujar();
+        // d3.Dibujar();
+        // d4.Dibujar();
+        // d5.Dibujar();
+        v.Dibujar();
+        refresh();
+        usleep(41000);
+    }
 
+    // Ventana v;
 
-
+    // v.Iniciar();
 
     return 0;
 }
-
